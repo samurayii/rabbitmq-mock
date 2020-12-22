c7ffa5ce3262428583cf423ff5a5fa5d04552020
Alexandr <samurayiii1986@gmail.com>
2020-12-23 00:58:15 +0300

    0.1.11

--- src/lib/input/index.ts
+++ src/lib/input/index.ts
@@ -211,8 +211,6 @@ export class Input extends EventEmitter implements IInput {
         this._channel.consume(this._config.queue.name, (message: Message) => {
 
             this._logger.log(`[Mock:${chalk.cyan(this._name)}:input] message received`, "dev");
-            this._logger.log(`[Mock:${chalk.cyan(this._name)}:input] Message:`, "debug");
-            this._logger.log(JSON.stringify(message, null, 2), "debug");
                     
             try {
               
